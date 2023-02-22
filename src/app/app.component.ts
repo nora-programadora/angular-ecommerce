2b6c58d1a19f6980ad4b947934b5490789d9624a
Nora-programadora <nora.programadora@gmail.com>
2023-02-21 19:52:28 -0600

    inout y outputs components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  imgParent = 'https://www.noraprogramadora.com/wp-content/uploads/2020/06/cropped-cropped-IMG_20170219_194544-e1592100658919.jpg'
   title = 'ecommerce';
+
+  onLoaded(img: string){
+    console.log('log padre' , img)
+  }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImgComponent implements OnInit {
 
+  @Input() img: string = '';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvf9wn1WvKWCp2eCV0atTl56ONzL6TyTPh702UMXqeHag2ZUG0YPch6-XWd2o4S_dK1J4&usqp=CAU'
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded(){
+    console.log('log hijo')
+    this.loaded.emit(this.img);
+  }
 }
