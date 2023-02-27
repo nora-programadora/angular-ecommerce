d38d7b3e04292f0734484d00a6b1109a2659a336
Nora-programadora <nora.programadora@gmail.com>
2023-02-27 17:20:16 -0600

    7 - ngDestroy and SetInput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Product } from './models/product.model';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  showImg = true;
   imgParent = 'https://www.noraprogramadora.com/wp-content/uploads/2020/06/cropped-cropped-IMG_20170219_194544-e1592100658919.jpg'
   title = 'ecommerce';
 
@@ -51,5 +52,9 @@ export class AppComponent {
 
   onLoaded(img: string){
     console.log('log padre' , img)
+  }
+
+  toggleImg() {
+    this.showImg = !this.showImg;
   }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy, SimpleChange, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -6,19 +6,32 @@ import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewIni
   styleUrls: ['./img.component.sass']
 })
 export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy{
+  img: string = '';
 
-  @Input() img: string = '';
+  @Input('img') 
+  set changeImg(newImg:string){
+    this.img = newImg;
+    console.log('change just img: ', this.img)
+    //code
+  }
+  @Input() alt: string = '';
   @Output() loaded = new EventEmitter<string>();
+
   imageDefault = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvf9wn1WvKWCp2eCV0atTl56ONzL6TyTPh702UMXqeHag2ZUG0YPch6-XWd2o4S_dK1J4&usqp=CAU'
+  counter = 0;
+  counterFn: number | undefined;
 
   constructor() {
     console.log('constructor', 'imgValue =>', this.img);
   }
 
-  ngOnChanges(): void {
+  ngOnChanges(changes: SimpleChanges): void {
     // before render and during render
     // change inputs - n times
     console.log('ngOnChanges', 'imgValue =>', this.img);
+    console.log('changes: ',changes);
+
+    // if(changes.img)
 
   }
 
@@ -26,6 +39,10 @@ export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy
     // before render
     // async - fetch -- once times
     console.log('ngOnInit');
+    this.counterFn = window.setInterval(() => {
+      this.counter += 1;
+      console.log('run counter')
+    }, 1000)
   }
 
   ngAfterViewInit(): void {
@@ -36,6 +53,7 @@ export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy
 
   ngOnDestroy(): void {
     //delete
+    window.clearInterval(this.counterFn);
   }
 
   imgError() {
